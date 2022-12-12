8aa118fb1f67b419af284bd11b8dce684bf90886
Yitong Wang <90431341+xxwwwww@users.noreply.github.com>
2022-12-12 17:29:05 +0000

    fastapi

--- main.py
+++ main.py
@@ -27,11 +27,10 @@ if header != None:
 connection.commit()
 
 
-# cursor.execute("SELECT * FROM vgsales limit 1")
-# print(cursor.fetchall())
+cursor.execute("SELECT Publisher, COUNT(Publisher) AS 'num_of_pub' FROM vgsales WHERE Year BETWEEN 2011 AND 2015 GROUP BY Publisher ORDER BY num_of_pub DESC LIMIT 10;")
+print(cursor.fetchall())
 
 connection.close()
-
 
 
 app = FastAPI()
@@ -41,11 +40,70 @@ async def root():
     return {"message": "Hello, welcome!"}
 
 # the top 10 most popular video games according to the global sales
-query1 = """SELECT * FROM vgsales limit 10;"""
+query1 = """SELECT Rank, Name, Platform, Year, Genre, Publisher, Global_Sales FROM vgsales limit 10;"""
 
 @app.get("/popular")
 async def popular():
     return {"top 10 most popular video games": "10"} 
+
+
+
+
+
+
+
+# The types of games that users preferred from 2011 to 2015, 2015-2020
+
+query2 = """
+    SELECT Genre, COUNT(Genre) AS 'num_of_genre' 
+    FROM vgsales 
+    WHERE Year BETWEEN 2011 AND 2015 
+    GROUP BY Genre 
+    ORDER BY num_of_genre DESC;
+"""
+query3 = """
+    SELECT Genre, COUNT(Genre) AS 'num_of_genre' 
+    FROM vgsales 
+    WHERE Year BETWEEN 2015 AND 2020 
+    GROUP BY Genre 
+    ORDER BY num_of_genre DESC;
+"""
+
+
+@app.get("/type")
+async def type1():
+    return {"2011-2015": "10"} 
+
+
+
+# Top 10 global publishers from 2011-2015, 2015-2020
+
+query4 = """
+    SELECT Publisher, COUNT(Publisher) AS 'num_of_pub' 
+    FROM vgsales 
+    WHERE Year BETWEEN 2011 AND 2015 
+    GROUP BY Publisher 
+    ORDER BY num_of_pub DESC
+    LIMIT 10;
+"""
+query5 = """
+    SELECT Publisher, COUNT(Publisher) AS 'num_of_pub' 
+    FROM vgsales 
+    WHERE Year BETWEEN 2015 AND 2020 
+    GROUP BY Publisher 
+    ORDER BY num_of_pub DESC
+    LIMIT 10;
+"""
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
